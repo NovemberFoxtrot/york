5da63a633e788d560ec457a4c1bb7023f45b8823
NovemberFoxtrot <november@fox.trot>
2013-09-12 01:48:58 -0700

    muck

--- public/js/thatscript.js
+++ public/js/thatscript.js
@@ -1,26 +1,25 @@
 var thatApp = angular.module("thatApp", []);
 
 thatApp.controller("thatCtrl", function($scope) {
-	$scope.leaveVoiceMessage = function(number, message) {
-		console.log(number, message)
-	}
-})
+	$scope.leaveVoicemail = function(number, message) {
+		console.log(number, message);
+	};
+});
 
 thatApp.directive("phone", function() {
 	return {
 		restrict: "E",
 		scope: {
-			number:   "@",
-			network:  "=",
-			makeCall: "&",
+			number:"@",
+			network:"=",
+			makeCall:"&"
 		},
-		template : '<div>{{number}}</div>' +
-		'<input type="text" ng-model="value">' +
-    '<div class="btn" ng-click="leaveVoiceMessage({number: number, message: value})"></div>' +
-		'',
+    template: '<div class="panel">Number: {{number}} </div>Network:<select ng-model="network" ng-options="net for net in networks">'+
+              '<input type="text" ng-model="value">' +
+              '<div class="button" ng-click="makeCall({number: number, message:value})">Call home!</div>',
 		link: function(scope) {
 			scope.networks = ["Verizon", "AT&T", "Pacific Bell"];
 			scope.network = scope.networks[0];
 		}
-	}
-})
+	};
+});

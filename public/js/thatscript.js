d2cfef0b0ebffaff74c2fee77826839f84e77bdb
NovemberFoxtrot <november@fox.trot>
2013-09-13 00:00:50 -0700

    ng

--- public/js/thatscript.js
+++ public/js/thatscript.js
@@ -1,25 +1,28 @@
 var thatApp = angular.module("thatApp", []);
 
-thatApp.controller("thatCtrl", function($scope) {
-	$scope.leaveVoicemail = function(number, message) {
-		console.log(number, message);
-	};
+thatApp.directive("dumbPassword", function () {
+  var validElement = angular.element('<div>{{model.input}}</div>');
+
+  var link = function (scope, element) {
+    scope.$watch("model.input", function (value) {
+      if(value === "password") {
+        console.log(element);
+        validElement.addClass("btn");
+      } else {
+        validElement.removeClass("btn");
+			}
+    });
+  };
+
+  return {
+    restrict: "E",
+    replace: true,
+    template: '<div><input type="text" ng-model="model.input">',
+    compile: function (tElem) {
+      tElem.append(validElement);
+        
+      return link;
+    }  
+  }; 
 });
 
-thatApp.directive("phone", function() {
-	return {
-		restrict: "E",
-		scope: {
-			number:"@",
-			network:"=",
-			makeCall:"&"
-		},
-    template: '<div class="panel">Number: {{number}} </div>Network:<select ng-model="network" ng-options="net for net in networks">'+
-              '<input type="text" ng-model="value">' +
-              '<div class="button" ng-click="makeCall({number: number, message:value})">Call home!</div>',
-		link: function(scope) {
-			scope.networks = ["Verizon", "AT&T", "Pacific Bell"];
-			scope.network = scope.networks[0];
-		}
-	};
-});

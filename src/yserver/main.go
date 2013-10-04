8af00f820b6dcd9f2850ab653cbec518aa0c4b53
NovemberFoxtrot <november@fox.trot>
2013-10-03 23:46:18 -0700

    runs

--- src/yserver/main.go
+++ src/yserver/main.go
@@ -4,9 +4,7 @@ import (
 	"net/http"
 	"os"
 	"yeasy"
-	"yindex"
 	"ytemplate"
-	"ytext"
 )
 
 type tv struct {
@@ -15,13 +13,7 @@ type tv struct {
 }
 
 type stv struct {
-	Result yindex.QueryResult
 	Score  int
-}
-
-func AddHandler(w http.ResponseWriter, r *http.Request) {
-	go yindex.Add(r.FormValue("website"))
-	http.Redirect(w, r, "/", http.StatusFound)
 }
 
 func MediaHandler(w http.ResponseWriter, r *http.Request) {
@@ -36,26 +28,13 @@ func ImageHandler(w http.ResponseWriter, r *http.Request) {
 func SearchHandler(w http.ResponseWriter, r *http.Request) {
 	ytemplate.ThePool.Fill("search", "templates/layout.html", "templates/search.html")
 
-	stvs := make([]stv, 0)
-
-	for index, location := range yindex.Query(r.FormValue("query")) {
-		stvs = append(stvs, stv{location, index})
-	}
-
-	ytemplate.ThePool.Pools["search"].Execute(w, stvs)
+	ytemplate.ThePool.Pools["search"].Execute(w, nil)
 }
 
 func IndexHandler(w http.ResponseWriter, r *http.Request) {
 	ytemplate.ThePool.Fill("index", "templates/layout.html", "templates/index.html")
 
-	tvs := make([]tv, 0)
-	tvs = append(tvs, tv{"Index", yindex.IndexDataLen()})
-
-	for i := 0; i < len(ytext.TheDocuments); i++ {
-		tvs = append(tvs, tv{ytext.TheDocuments[i].Location, len(ytext.TheDocuments[i].Grams)})
-	}
-
-	ytemplate.ThePool.Pools["index"].Execute(w, tvs)
+	ytemplate.ThePool.Pools["index"].Execute(w, nil)
 }
 
 func New() {
@@ -63,7 +42,6 @@ func New() {
 	yeasy.CheckError(err)
 
 	http.HandleFunc("/", IndexHandler)
-	http.HandleFunc("/add", AddHandler)
 	http.HandleFunc("/images/", ImageHandler)
 	http.HandleFunc("/media", MediaHandler)
 	http.HandleFunc("/search", SearchHandler)
